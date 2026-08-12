93330a268411322a99420dd7a86bee1293c0c58c
Aralf-Z <candats@outlook.com>
2026-08-13 00:51:01 +0800

    [chore] 文件结构

--- Sanmon.bat
+++ Sanmon.bat
@@ -1,5 +1,5 @@
 @echo off
 set EDITOR_PATH=C:\Program Files\Unity\Unity 2022.3.62f3\Editor\Unity.exe
-set PROJECT_PATH=D:\Projects\unity\Sanmon Shosetsu\Client
+set PROJECT_PATH=D:\Projects\unity\Sanmon-Shosetsu\client
 
 start "" "%EDITOR_PATH%" -- --projectPath "%PROJECT_PATH%"
